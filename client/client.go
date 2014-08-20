9a4cebc298371827854b7571130ef0dad460124b
dr.max <maximilien@acm.org>
2014-08-20 16:05:43 -0700

    added SL client * based on packer-builder-softlayer/.../client.go * added basic unit test

---
+++ client/client.go
@@ -0,0 +1,120 @@
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"text/template"
+)
+
+const SOFTLAYER_API_URL = "api.softlayer.com/rest/v3"
+
+type softLayerClient struct {
+	httpClient *http.Client
+
+	username string
+	apiKey   string
+
+	templatePath string
+}
+
+func NewSoftLayerClient(username, apiKey string) *softLayerClient {
+	pwd, _ := os.Getwd()
+	return &softLayerClient{
+		username:     username,
+		apiKey:       apiKey,
+		templatePath: pwd,
+		httpClient: &http.Client{
+			Transport: &http.Transport{
+				Proxy: http.ProxyFromEnvironment,
+			},
+		},
+	}
+}
+
+func (slc *softLayerClient) generateRequestBody(templateData interface{}) (*bytes.Buffer, error) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	bodyTemplate := template.Must(template.ParseFiles(filepath.Join(cwd, slc.templatePath)))
+	body := new(bytes.Buffer)
+	bodyTemplate.Execute(body, templateData)
+
+	log.Printf("Generated request body %s", body)
+
+	return body, nil
+}
+
+func (slc *softLayerClient) hasErrors(body map[string]interface{}) error {
+	if errString, ok := body["error"]; !ok {
+		return nil
+	} else {
+		return errors.New(errString.(string))
+	}
+}
+
+func (slc *softLayerClient) doRawHttpRequest(path string, requestType string, requestBody *bytes.Buffer) ([]byte, error) {
+	url := fmt.Sprintf("https://%s:%s@%s/%s", slc.username, slc.apiKey, SOFTLAYER_API_URL, path)
+	log.Printf("Sending new request to softlayer: %s", url)
+
+	var lastResponse http.Response
+	switch requestType {
+	case "POST", "DELETE":
+		req, err := http.NewRequest(requestType, url, requestBody)
+
+		if err != nil {
+			return nil, err
+		}
+		resp, err := slc.httpClient.Do(req)
+
+		if err != nil {
+			return nil, err
+		} else {
+			lastResponse = *resp
+		}
+	case "GET":
+		resp, err := http.Get(url)
+
+		if err != nil {
+			return nil, err
+		} else {
+			lastResponse = *resp
+		}
+	default:
+		return nil, errors.New(fmt.Sprintf("Undefined request type '%s', only GET/POST/DELETE are available!", requestType))
+	}
+
+	responseBody, err := ioutil.ReadAll(lastResponse.Body)
+	lastResponse.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("Received response from SoftLayer: %s", responseBody)
+
+	return responseBody, nil
+}
+
+func (slc *softLayerClient) doHttpRequest(path string, requestType string, requestBody *bytes.Buffer) (map[string]interface{}, error) {
+	responseBody, err := slc.doRawHttpRequest(path, requestType, requestBody)
+	if err != nil {
+		err := errors.New(fmt.Sprintf("Failed to get proper HTTP response from SoftLayer API"))
+		return nil, err
+	}
+
+	var decodedResponse map[string]interface{}
+	err = json.Unmarshal(responseBody, &decodedResponse)
+	if err != nil {
+		err := errors.New(fmt.Sprintf("Failed to decode JSON response from SoftLayer: %s | %s", responseBody, err))
+		return nil, err
+	}
+
+	return decodedResponse, nil
+}
---
+++ client/client_test.go
@@ -0,0 +1,32 @@
+package client_test
+
+import (
+	"os"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	client "github.com/maximilien/softlayer-go/client"
+)
+
+var _ = Describe("softLayerClient", func() {
+	var (
+		username string
+		apiKey   string
+	)
+
+	BeforeEach(func() {
+		username = os.Getenv("SL_USERNAME")
+		apiKey = os.Getenv("SL_API_KEY")
+	})
+
+	Context("New", func() {
+		It("creates a new client with username and apiKey", func() {
+			Expect(username).ToNot(Equal(""), "username cannot be empty, set SL_USERNAME")
+			Expect(apiKey).ToNot(Equal(""), "apiKey cannot be empty, set SL_API_KEY")
+			client := client.NewSoftLayerClient(username, apiKey)
+			Expect(client).ToNot(BeNil())
+		})
+	})
+
+})
