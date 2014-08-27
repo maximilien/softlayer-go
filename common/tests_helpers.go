44707e4f33ba73fce61828dbf425853b920ab465
dr.max <maximilien@acm.org>
2014-08-27 15:05:18 -0700

    added simple common/test_helpers and test_fixtures/*.json files

---
+++ common/tests_helpers.go
@@ -0,0 +1,12 @@
+package test_helpers
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+func ReadJsonTestFixtures(packageName, fileName string) ([]byte, error) {
+	wd, _ := os.Getwd()
+	return ioutil.ReadFile(filepath.Join(wd, "..", "test_fixtures", packageName, fileName))
+}
