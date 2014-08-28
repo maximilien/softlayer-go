c553f555dc422ff9af06a505c197ab4a2fd13bf9
dr.max <maximilien@acm.org>
2014-08-28 11:21:24 -0700

    figured out how to parse JSON nulls with * (pointer) types

--- data_types/softlayer_virtual_guest.go
+++ data_types/softlayer_virtual_guest.go
@@ -5,26 +5,26 @@ import (
 )
 
 type SoftLayer_Virtual_Guest struct {
-	AccountId                    int       `json:"accountId"`
-	CreateDate                   time.Time `json:"createDate"`
-	DedicatedAccountHostOnlyFlag bool      `json:"dedicatedAccountHostOnlyFlag"`
-	Domain                       string    `json:"domain"`
-	FullyQualifiedDomainName     string    `json:"fullyQualifiedDomainName"`
-	Hostname                     string    `json:"hostname"`
-	Id                           int       `json:"id"`
-	LastPowerStateId             int       `json:"lastPowerStateId"`
-	//LastVerifiedDate             time.Time 	`json:"lastVerifiedDate"`
-	MaxCpu      int    `json:"maxCpu"`
-	MaxCpuUnits string `json:"maxCpuUnits"`
-	MaxMemory   int    `json:"maxMemory"`
-	//MetricPollDate               time.Time  `json:"metricPollDate"`
-	ModifyDate             time.Time `json:"modifyDate"`
-	Notes                  string    `json:"notes"`
-	PostInstallScriptUri   string    `json:"postInstallScriptUri"`
-	PrivateNetworkOnlyFlag bool      `json:"privateNetworkOnlyFlag"`
-	StartCpus              int       `json:"startCpus"`
-	StatusId               int       `json:"statusId"`
-	Uuid                   string    `json:"uuid"`
+	AccountId                    int        `json:"accountId"`
+	CreateDate                   *time.Time `json:"createDate"`
+	DedicatedAccountHostOnlyFlag bool       `json:"dedicatedAccountHostOnlyFlag"`
+	Domain                       string     `json:"domain"`
+	FullyQualifiedDomainName     string     `json:"fullyQualifiedDomainName"`
+	Hostname                     string     `json:"hostname"`
+	Id                           int        `json:"id"`
+	LastPowerStateId             int        `json:"lastPowerStateId"`
+	LastVerifiedDate             *time.Time `json:"lastVerifiedDate"`
+	MaxCpu                       int        `json:"maxCpu"`
+	MaxCpuUnits                  string     `json:"maxCpuUnits"`
+	MaxMemory                    int        `json:"maxMemory"`
+	MetricPollDate               *time.Time `json:"metricPollDate"`
+	ModifyDate                   *time.Time `json:"modifyDate"`
+	Notes                        string     `json:"notes"`
+	PostInstallScriptUri         string     `json:"postInstallScriptUri"`
+	PrivateNetworkOnlyFlag       bool       `json:"privateNetworkOnlyFlag"`
+	StartCpus                    int        `json:"startCpus"`
+	StatusId                     int        `json:"statusId"`
+	Uuid                         string     `json:"uuid"`
 }
 
 type SoftLayer_Virtual_Guest_Template struct {
