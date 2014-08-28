fd80aa3e5b8e4fdb99554255abd90b7f8addae65
dr.max <maximilien@acm.org>
2014-08-28 15:30:43 -0700

    added integration tests for SL_Account methods

--- integration/services/services_test.go
+++ integration/services/services_test.go
@@ -1,13 +1,89 @@
 package services_test
 
 import (
+	"os"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
+	slclient "github.com/maximilien/softlayer-go/client"
+	softlayer "github.com/maximilien/softlayer-go/softlayer"
 )
 
 var _ = Describe("SoftLayer Services", func() {
-	Context("SoftLayer_Account", func() {
-		It("returs an array of SoftLayer_Virtual_Guest objects", func() {
+	var (
+		username, apiKey string
+		err              error
+
+		client softlayer.Client
+
+		accountService      softlayer.SoftLayer_Account_Service
+		virtualGuestService softlayer.SoftLayer_Virtual_Guest_Service
+	)
+
+	BeforeEach(func() {
+		username = os.Getenv("SL_USERNAME")
+		Expect(username).ToNot(Equal(""))
+
+		apiKey = os.Getenv("SL_API_KEY")
+		Expect(apiKey).ToNot(Equal(""))
+
+		client = slclient.NewSoftLayerClient(username, apiKey)
+		Expect(client).ToNot(BeNil())
+
+		accountService, err = client.GetSoftLayer_Account_Service()
+		Expect(err).ToNot(HaveOccurred())
+		Expect(accountService).ToNot(BeNil())
+
+		virtualGuestService, err = client.GetSoftLayer_Virtual_Guest_Service()
+		Expect(err).ToNot(HaveOccurred())
+		Expect(virtualGuestService).ToNot(BeNil())
+	})
+
+	Context("uses SoftLayer_Account to list current virtual: disk images, guests, and network storage", func() {
+		It("returns an array of SoftLayer_Virtual_Guest disk images", func() {
+			virtualDiskImages, err := accountService.GetVirtualDiskImages()
+			Expect(err).ToNot(HaveOccurred())
+			Expect(len(virtualDiskImages)).To(BeNumerically(">=", 0))
+		})
+
+		It("returns an array of SoftLayer_Virtual_Guest objects", func() {
+			virtualGuests, err := accountService.GetVirtualGuests()
+			Expect(err).ToNot(HaveOccurred())
+			Expect(len(virtualGuests)).To(BeNumerically(">=", 0))
+		})
+
+		It("returns an array of SoftLayer_Virtual_Guest network storage", func() {
+			networkStorageArray, err := accountService.GetNetworkStorage()
+			Expect(err).ToNot(HaveOccurred())
+			Expect(len(networkStorageArray)).To(BeNumerically(">=", 0))
+		})
+	})
+
+	XContext("uses SoftLayer_Account to create and then delete a virtual guest instance", func() {
+		It("creates the virtual guest instance and waits for it to be active", func() {
+			Expect(false).To(BeTrue())
+		})
+
+		It("deletes the virtual guest instance if it is running", func() {
+			Expect(false).To(BeTrue())
+		})
+	})
+
+	XContext("uses SoftLayer_Account to create a new instance and network storage and attach them", func() {
+		It("creates the virtual guest instance and waits for it to be active", func() {
+			Expect(false).To(BeTrue())
+		})
+
+		It("creates the disk storage and attaches it to the instance", func() {
+			Expect(false).To(BeTrue())
+		})
+
+		It("deletes the virtual guest instance if it is running", func() {
+			Expect(false).To(BeTrue())
+		})
+
+		It("detaches and deletes the network storage if available", func() {
 			Expect(false).To(BeTrue())
 		})
 	})
