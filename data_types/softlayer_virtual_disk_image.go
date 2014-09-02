d785f770ce800f2d6d3be54efd87ddb56b259840
dr.max <maximilien@acm.org>
2014-09-02 16:49:57 -0700

    changed time.Time field to be *time.Time so that null can be parsed correctly

--- data_types/softlayer_virtual_disk_image.go
+++ data_types/softlayer_virtual_disk_image.go
@@ -5,16 +5,16 @@ import (
 )
 
 type SoftLayer_Virtual_Disk_Image struct {
-	Capacity    int       `json:"capacity"`
-	Checksum    string    `json:"checksum"`
-	CreateDate  time.Time `json:"createDate"`
-	Description string    `json:"description"`
-	Id          int       `json:"id"`
-	//ModifyDate          time.Time 	`json:"modifyDate"`
-	Name                string `json:"name"`
-	ParentId            int    `json:"parentId"`
-	StorageRepositoryId int    `json:"storageRepositoryId"`
-	TypeId              int    `json:"typeId"`
-	Units               string `json:"units"`
-	Uuid                string `json:"uuid"`
+	Capacity            int        `json:"capacity"`
+	Checksum            string     `json:"checksum"`
+	CreateDate          *time.Time `json:"createDate"`
+	Description         string     `json:"description"`
+	Id                  int        `json:"id"`
+	ModifyDate          *time.Time `json:"modifyDate"`
+	Name                string     `json:"name"`
+	ParentId            int        `json:"parentId"`
+	StorageRepositoryId int        `json:"storageRepositoryId"`
+	TypeId              int        `json:"typeId"`
+	Units               string     `json:"units"`
+	Uuid                string     `json:"uuid"`
 }
