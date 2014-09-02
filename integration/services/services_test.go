7dfa7f86e675cabc18de70dc8b9dbfcbdd450297
dr.max <maximilien@acm.org>
2014-09-02 16:50:49 -0700

    implemented the test helper methods to find test resouces

--- integration/services/services_test.go
+++ integration/services/services_test.go
@@ -3,6 +3,7 @@ package services_test
 import (
 	"errors"
 	"os"
+	"strings"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -112,15 +113,87 @@ var _ = Describe("SoftLayer Services", func() {
 })
 
 func findTestVirtualGuests() ([]datatypes.SoftLayer_Virtual_Guest, error) {
-	return []datatypes.SoftLayer_Virtual_Guest{}, nil
+	accountService, err := createAccountService()
+	if err != nil {
+		return []datatypes.SoftLayer_Virtual_Guest{}, err
+	}
+
+	virtualGuests, err := accountService.GetVirtualGuests()
+	if err != nil {
+		return []datatypes.SoftLayer_Virtual_Guest{}, err
+	}
+
+	testVirtualGuests := []datatypes.SoftLayer_Virtual_Guest{}
+	for _, vGuest := range virtualGuests {
+		if strings.Contains(vGuest.Notes, TEST_NOTES_PREFIX) {
+			testVirtualGuests = append(testVirtualGuests, vGuest)
+		}
+	}
+
+	return testVirtualGuests, nil
+}
+
+func findTestVirtualDiskImages() ([]datatypes.SoftLayer_Virtual_Disk_Image, error) {
+	accountService, err := createAccountService()
+	if err != nil {
+		return []datatypes.SoftLayer_Virtual_Disk_Image{}, err
+	}
+
+	virtualDiskImages, err := accountService.GetVirtualDiskImages()
+	if err != nil {
+		return []datatypes.SoftLayer_Virtual_Disk_Image{}, err
+	}
+
+	testVirtualDiskImages := []datatypes.SoftLayer_Virtual_Disk_Image{}
+	for _, vDI := range virtualDiskImages {
+		if strings.Contains(vDI.Description, TEST_NOTES_PREFIX) {
+			testVirtualDiskImages = append(testVirtualDiskImages, vDI)
+		}
+	}
+
+	return testVirtualDiskImages, nil
 }
 
 func findTestNetworkStorage() ([]datatypes.SoftLayer_Network_Storage, error) {
-	return []datatypes.SoftLayer_Network_Storage{}, nil
+	accountService, err := createAccountService()
+	if err != nil {
+		return []datatypes.SoftLayer_Network_Storage{}, err
+	}
+
+	networkStorageArray, err := accountService.GetNetworkStorage()
+	if err != nil {
+		return []datatypes.SoftLayer_Network_Storage{}, err
+	}
+
+	testNetworkStorageArray := []datatypes.SoftLayer_Network_Storage{}
+	for _, storage := range networkStorageArray {
+		if strings.Contains(storage.Notes, TEST_NOTES_PREFIX) {
+			testNetworkStorageArray = append(testNetworkStorageArray, storage)
+		}
+	}
+
+	return testNetworkStorageArray, nil
 }
 
 func findTestSshKeys() ([]datatypes.SoftLayer_Ssh_Key, error) {
-	return []datatypes.SoftLayer_Ssh_Key{}, nil
+	accountService, err := createAccountService()
+	if err != nil {
+		return []datatypes.SoftLayer_Ssh_Key{}, err
+	}
+
+	sshKeys, err := accountService.GetSshKeys()
+	if err != nil {
+		return []datatypes.SoftLayer_Ssh_Key{}, err
+	}
+
+	testSshKeys := []datatypes.SoftLayer_Ssh_Key{}
+	for _, key := range sshKeys {
+		if strings.Contains(key.Notes, TEST_NOTES_PREFIX) {
+			testSshKeys = append(testSshKeys, key)
+		}
+	}
+
+	return testSshKeys, nil
 }
 
 func getUsernameAndApiKey() (string, string, error) {
