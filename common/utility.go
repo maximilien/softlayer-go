b7e9ba9666e4f809cf3a324bc8e7391b25634037
dr.max <maximilien@acm.org>
2015-07-29 14:54:44 -0700

    corrected Go style

--- common/utility.go
+++ common/utility.go
@@ -6,9 +6,11 @@ import (
 
 func ValidateJson(s string) (bool, error) {
 	var js map[string]interface{}
+
 	err := json.Unmarshal([]byte(s), &js)
 	if err != nil {
 		return false, err
 	}
+
 	return true, nil
 }
