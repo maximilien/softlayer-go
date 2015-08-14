b3f1ad4f7a7c93f34de4c5538f5d7be9123356be
jianqiu <wangjq@cn.ibm.com>
2015-08-14 17:28:37 +0800

    debug

--- services/softlayer_product_order_test.go
+++ services/softlayer_product_order_test.go
@@ -60,7 +60,7 @@ var _ = Describe("SoftLayer_Product_Order", func() {
 
 	Context("#PlaceContainerOrderNetworkPerformanceStorageIscsi", func() {
 		BeforeEach(func() {
-			fakeClient.DoRawHttpRequestResponse, err = testhelpers.ReadJsonTestFixtures("services", "SoftLayer_Product_Order_placeContainerOrderNetworkPerformanceStorageIscsi.json")
+			fakeClient.DoRawHttpRequestResponse, err = testhelpers.ReadJsonTestFixtures("services", "SoftLayer_Product_Order_placeOrder.json")
 			Expect(err).ToNot(HaveOccurred())
 		})
 
@@ -74,7 +74,7 @@ var _ = Describe("SoftLayer_Product_Order", func() {
 
 	Context("#PlaceContainerOrderVirtualGuestUpgrade", func() {
 		BeforeEach(func() {
-			fakeClient.DoRawHttpRequestResponse, err = testhelpers.ReadJsonTestFixtures("services", "SoftLayer_Product_Order_placeContainerOrderVirtualGuestUpgrade.json")
+			fakeClient.DoRawHttpRequestResponse, err = testhelpers.ReadJsonTestFixtures("services", "SoftLayer_Product_Order_placeOrder.json")
 			Expect(err).ToNot(HaveOccurred())
 		})
 
