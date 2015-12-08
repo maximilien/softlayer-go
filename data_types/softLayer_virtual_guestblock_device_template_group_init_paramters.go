9afe56f812ad45c053f2158ee63103db645d66d7
dr.max <maximilien@acm.org>
2015-12-08 15:20:09 +0800

    renamed to correct name *_guestblock_* to *_guest_block_*

--- data_types/softLayer_virtual_guestblock_device_template_group_init_paramters.go
+++
@@ -1,9 +0,0 @@
-package data_types
-
-type SoftLayer_Virtual_Guest_Block_Device_Template_GroupInitParameters struct {
-	Parameters SoftLayer_Virtual_Guest_Block_Device_Template_GroupInitParameter `json:"parameters"`
-}
-
-type SoftLayer_Virtual_Guest_Block_Device_Template_GroupInitParameter struct {
-	AccountId int `json:"accountId"`
-}
