798461536448db733e0e9740540829833e9af745
Eduard Bakaev <bakaeved@gmail.com>
2015-12-08 12:29:49 +0300

    #8 CreateObjects method implemented for bulk records creation. It needs for re-create DNS to keep existing records.

--- softlayer/softLayer_dns_domain_record_service.go
+++ softlayer/softLayer_dns_domain_record_service.go
@@ -8,6 +8,7 @@ type SoftLayer_Dns_Domain_Record_Service interface {
 	Service
 
 	CreateObject(template datatypes.SoftLayer_Dns_Domain_Record_Template) (datatypes.SoftLayer_Dns_Domain_Record, error)
+	CreateObjects(template []datatypes.SoftLayer_Dns_Domain_Record_Template) ([]datatypes.SoftLayer_Dns_Domain_Record, error)
 	GetObject(recordId int) (datatypes.SoftLayer_Dns_Domain_Record, error)
 	DeleteObject(recordId int) (bool, error)
 	EditObject(recordId int, template datatypes.SoftLayer_Dns_Domain_Record) (bool, error)
