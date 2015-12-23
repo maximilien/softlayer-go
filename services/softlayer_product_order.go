1cdb215b75a87fae5b8d18708a5761c5d96f0183
igoonich <igoonich@evelopers.com>
2015-12-23 16:23:22 +0300

    #21 Remove boilerplate code for placing orders

--- services/softlayer_product_order.go
+++ services/softlayer_product_order.go
@@ -29,24 +29,7 @@ func (slpo *softLayer_Product_Order_Service) PlaceOrder(order datatypes.SoftLaye
 			order,
 		},
 	}
-
-	requestBody, err := json.Marshal(parameters)
-	if err != nil {
-		return datatypes.SoftLayer_Container_Product_Order_Receipt{}, err
-	}
-
-	responseBytes, err := slpo.client.DoRawHttpRequest(fmt.Sprintf("%s/placeOrder.json", slpo.GetName()), "POST", bytes.NewBuffer(requestBody))
-	if err != nil {
-		return datatypes.SoftLayer_Container_Product_Order_Receipt{}, err
-	}
-
-	receipt := datatypes.SoftLayer_Container_Product_Order_Receipt{}
-	err = json.Unmarshal(responseBytes, &receipt)
-	if err != nil {
-		return datatypes.SoftLayer_Container_Product_Order_Receipt{}, err
-	}
-
-	return receipt, nil
+	return slpo.placeOrder(parameters)
 }
 
 func (slpo *softLayer_Product_Order_Service) PlaceContainerOrderNetworkPerformanceStorageIscsi(order datatypes.SoftLayer_Container_Product_Order_Network_PerformanceStorage_Iscsi) (datatypes.SoftLayer_Container_Product_Order_Receipt, error) {
@@ -55,24 +38,7 @@ func (slpo *softLayer_Product_Order_Service) PlaceContainerOrderNetworkPerforman
 			order,
 		},
 	}
-
-	requestBody, err := json.Marshal(parameters)
-	if err != nil {
-		return datatypes.SoftLayer_Container_Product_Order_Receipt{}, err
-	}
-
-	responseBytes, err := slpo.client.DoRawHttpRequest(fmt.Sprintf("%s/placeOrder.json", slpo.GetName()), "POST", bytes.NewBuffer(requestBody))
-	if err != nil {
-		return datatypes.SoftLayer_Container_Product_Order_Receipt{}, err
-	}
-
-	receipt := datatypes.SoftLayer_Container_Product_Order_Receipt{}
-	err = json.Unmarshal(responseBytes, &receipt)
-	if err != nil {
-		return datatypes.SoftLayer_Container_Product_Order_Receipt{}, err
-	}
-
-	return receipt, nil
+	return slpo.placeOrder(parameters)
 }
 
 func (slpo *softLayer_Product_Order_Service) PlaceContainerOrderVirtualGuestUpgrade(order datatypes.SoftLayer_Container_Product_Order_Virtual_Guest_Upgrade) (datatypes.SoftLayer_Container_Product_Order_Receipt, error) {
@@ -81,24 +47,7 @@ func (slpo *softLayer_Product_Order_Service) PlaceContainerOrderVirtualGuestUpgr
 			order,
 		},
 	}
-
-	requestBody, err := json.Marshal(parameters)
-	if err != nil {
-		return datatypes.SoftLayer_Container_Product_Order_Receipt{}, err
-	}
-
-	responseBytes, err := slpo.client.DoRawHttpRequest(fmt.Sprintf("%s/placeOrder.json", slpo.GetName()), "POST", bytes.NewBuffer(requestBody))
-	if err != nil {
-		return datatypes.SoftLayer_Container_Product_Order_Receipt{}, err
-	}
-
-	receipt := datatypes.SoftLayer_Container_Product_Order_Receipt{}
-	err = json.Unmarshal(responseBytes, &receipt)
-	if err != nil {
-		return datatypes.SoftLayer_Container_Product_Order_Receipt{}, err
-	}
-
-	return receipt, nil
+	return slpo.placeOrder(parameters)
 }
 
 func (slpo *softLayer_Product_Order_Service) PlaceContainerOrderApplicationDeliveryController(order datatypes.SoftLayer_Container_Product_Order_Network_Application_Delivery_Controller) (datatypes.SoftLayer_Container_Product_Order_Receipt, error) {
@@ -107,7 +56,13 @@ func (slpo *softLayer_Product_Order_Service) PlaceContainerOrderApplicationDeliv
 			order,
 		},
 	}
+	return slpo.placeOrder(parameters)
+}
 
+
+// Private methods
+
+func (slpo *softLayer_Product_Order_Service) placeOrder(parameters interface{}) (datatypes.SoftLayer_Container_Product_Order_Receipt, error) {
 	requestBody, err := json.Marshal(parameters)
 	if err != nil {
 		return datatypes.SoftLayer_Container_Product_Order_Receipt{}, err
