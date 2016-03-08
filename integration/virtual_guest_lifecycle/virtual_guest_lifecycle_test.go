9e200c6a8935a5c9ef684161424a49a33f5f2bec
Matt Cui <mattcui@gmail.com>
2016-03-09 00:54:58 +0800

    Change the disk size to 10*1024

--- integration/virtual_guest_lifecycle/virtual_guest_lifecycle_test.go
+++ integration/virtual_guest_lifecycle/virtual_guest_lifecycle_test.go
@@ -94,7 +94,7 @@ var _ = Describe("SoftLayer Virtual Guest Lifecycle", func() {
 			virtualGuestService, err := testhelpers.CreateVirtualGuestService()
 			Expect(err).ToNot(HaveOccurred())
 
-			available, err := virtualGuestService.CheckHostDiskAvailability(virtualGuest.Id, 1024)
+			available, err := virtualGuestService.CheckHostDiskAvailability(virtualGuest.Id, 10*1024)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(available).To(BeTrue())
 		})
