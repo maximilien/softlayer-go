d67fa4d0321818d366e71ee36f2631a2bed0fa3b
Tim Mulhern <tim.mulhern.contractor@weather.com>
2016-06-20 12:12:03 -0400

    Add helpers for retrieving IDs for health check type, routing method, and routing type

--- common/helpers.go
+++ common/helpers.go
@@ -10,7 +10,10 @@ import (
 )
 
 const (
-	DATACENTER_TYPE_NAME = "SoftLayer_Location_Datacenter"
+	DATACENTER_TYPE_NAME   = "SoftLayer_Location_Datacenter"
+	ROUTING_TYPE_NAME      = "SoftLayer_Network_Application_Delivery_Controller_LoadBalancer_Routing_Type"
+	ROUTING_METHOD_NAME    = "SoftLayer_Network_Application_Delivery_Controller_LoadBalancer_Routing_Method"
+	HEALTH_CHECK_TYPE_NAME = "SoftLayer_Network_Application_Delivery_Controller_LoadBalancer_Health_Check_Type"
 )
 
 func GetDatacenterByName(client softlayer.Client, name string) (int, error) {
@@ -40,4 +43,91 @@ func GetDatacenterByName(client softlayer.Client, name string) (int, error) {
 	}
 
 	return -1, fmt.Errorf("Datacenter %s not found", name)
+}
+
+func GetRoutingTypeByName(client softlayer.Client, name string) (int, error) {
+	ObjectFilter := string(`{"keyname":{"operation":"` + name + `"}}`)
+	ObjectMasks := []string{"id", "keyname"}
+
+	response, errorCode, err := client.GetHttpClient().DoRawHttpRequestWithObjectFilterAndObjectMask(fmt.Sprintf("%s/getAllObjects.json", ROUTING_TYPE_NAME), ObjectMasks, ObjectFilter, "GET", new(bytes.Buffer))
+	if err != nil {
+		return -1, err
+	}
+
+	if IsHttpErrorCode(errorCode) {
+		errorMessage := fmt.Sprintf("softlayer-go: could not retrieve routing types, HTTP error code: '%d'", errorCode)
+		return -1, errors.New(errorMessage)
+	}
+
+	routingTypes := []datatypes.SoftLayer_Routing_Type{}
+	err = json.Unmarshal(response, &routingTypes)
+	if err != nil {
+		return -1, err
+	}
+
+	for _, routingType := range routingTypes {
+		if routingType.KeyName == name {
+			return routingType.Id, nil
+		}
+	}
+
+	return -1, fmt.Errorf("Routing type %s not found", name)
+}
+
+func GetRoutingMethodByName(client softlayer.Client, name string) (int, error) {
+	ObjectFilter := string(`{"keyname":{"operation":"` + name + `"}}`)
+	ObjectMasks := []string{"id", "keyname"}
+
+	response, errorCode, err := client.GetHttpClient().DoRawHttpRequestWithObjectFilterAndObjectMask(fmt.Sprintf("%s/getAllObjects.json", ROUTING_METHOD_NAME), ObjectMasks, ObjectFilter, "GET", new(bytes.Buffer))
+	if err != nil {
+		return -1, err
+	}
+
+	if IsHttpErrorCode(errorCode) {
+		errorMessage := fmt.Sprintf("softlayer-go: could not retrieve routing methods, HTTP error code: '%d'", errorCode)
+		return -1, errors.New(errorMessage)
+	}
+
+	routingMethods := []datatypes.SoftLayer_Routing_Method{}
+	err = json.Unmarshal(response, &routingMethods)
+	if err != nil {
+		return -1, err
+	}
+
+	for _, routingMethod := range routingMethods {
+		if routingMethod.KeyName == name {
+			return routingMethod.Id, nil
+		}
+	}
+
+	return -1, fmt.Errorf("Routing method %s not found", name)
+}
+
+func GetHealthCheckTypeByName(client softlayer.Client, name string) (int, error) {
+	ObjectFilter := string(`{"keyname":{"operation":"` + name + `"}}`)
+	ObjectMasks := []string{"id", "keyname"}
+
+	response, errorCode, err := client.GetHttpClient().DoRawHttpRequestWithObjectFilterAndObjectMask(fmt.Sprintf("%s/getAllObjects.json", HEALTH_CHECK_TYPE_NAME), ObjectMasks, ObjectFilter, "GET", new(bytes.Buffer))
+	if err != nil {
+		return -1, err
+	}
+
+	if IsHttpErrorCode(errorCode) {
+		errorMessage := fmt.Sprintf("softlayer-go: could not retrieve health check types, HTTP error code: '%d'", errorCode)
+		return -1, errors.New(errorMessage)
+	}
+
+	healthCheckTypes := []datatypes.SoftLayer_Health_Check_Type{}
+	err = json.Unmarshal(response, &healthCheckTypes)
+	if err != nil {
+		return -1, err
+	}
+
+	for _, healthCheckType := range healthCheckTypes {
+		if healthCheckType.KeyName == name {
+			return healthCheckType.Id, nil
+		}
+	}
+
+	return -1, fmt.Errorf("Health check type %s not found", name)
 }
